97101b51217657dc6a3814b65c97ee1d077c3d71
Zygar <a@andywilkinson.co.nz>
2017-11-07 13:12:12 +1300

    excludes unneeded weights of Lato

--- src/app.js
+++ src/app.js
@@ -11,15 +11,15 @@ var main = require("./js/main");
 
    /* Comment out fonts you don't want in the final bundle. */
    
-import LatoHairline from 'Fonts/lato-hairline/lato-hairline.css';  // 100
-import LatoThin from 'Fonts/lato-thin/lato-thin.css'; // 200
-import LatoLight from 'Fonts/lato-light/lato-light.css'; // 300
+// import LatoHairline from 'Fonts/lato-hairline/lato-hairline.css';  // 100
+// import LatoThin from 'Fonts/lato-thin/lato-thin.css'; // 200
+// import LatoLight from 'Fonts/lato-light/lato-light.css'; // 300
 import LatoRegular from 'Fonts/lato-regular/lato-regular.css';  // 400 / REGULAR
-import LatoMedium from 'Fonts/lato-medium/lato-medium.css';  // 500
-import LatoSemibold from 'Fonts/lato-semibold/lato-semibold.css';   // 600
+// import LatoMedium from 'Fonts/lato-medium/lato-medium.css';  // 500
+// import LatoSemibold from 'Fonts/lato-semibold/lato-semibold.css';   // 600
 import LatoBold from 'Fonts/lato-bold/lato-bold.css'; // 700 / BOLD
-import LatoHeavy from 'Fonts/lato-heavy/lato-heavy.css';  // 800
-import LatoBlack from 'Fonts/lato-black/lato-black.css';  // 900 
+// import LatoHeavy from 'Fonts/lato-heavy/lato-heavy.css';  // 800
+// import LatoBlack from 'Fonts/lato-black/lato-black.css';  // 900 
 
 /* Load Styles */ 
 import styles from './css/main.css';
