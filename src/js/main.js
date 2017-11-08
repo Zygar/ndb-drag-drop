107ae4aa660d19c4797942dc6051ec0238ec4fc7
Zygar <a@andywilkinson.co.nz>
2017-11-08 22:08:19 +1300

    Working cut of a drag and drop which "magnetizes" to its dropzone. There is some very basic answer checking logic in there too.

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,83 @@
-var draggable = require('webpack-jquery-ui/draggable');
-var droppable = require('webpack-jquery-ui/droppable');
-var touchpunch = require('jquery-ui-touch-punch');
+var dragula = require("dragula");
+window.addEventListener( 'touchmove', function() {})
 
 
-// var sources = document.querySelector('.draggable');
-// var destinations = document.querySelector('.droppable');
 
-// console.log(sources,destinations)
-// dragula([sources,destinations])
+    var source = Array.from(document.querySelectorAll('.draggable-container'));
+    var dest = Array.from(document.querySelectorAll('.droppable'));
+    var draggables = source.concat(dest);
+    // console.log(source, dest)
 
-var $sources = $(".draggable");
-var $destinations = $(".droppable");
-$sources.draggable({
-    // revert:true,
-    snap: '.droppable'
 
+
+var draggable = dragula({
+    isContainer: function (el) {
+        return el.classList.contains('isdraggable');
+    },
+    accepts: function(el, target, source, sibling) {
+        if (target.classList.contains('draggable-container')) {
+            return false
+        } else if (target.classList.contains('droppable')) {
+            console.log("can drop here")
+            return true
+        }
+        // console.log(el, target, source, sibling)
+    }
 });
-$destinations.droppable({
-    accept: ".draggable",
-    hoverClass: "drop-hover"
+
+draggable.on('drag', function(el){
+    $(document).on('touchstart', function(e) {
+        e.preventDefault();
+    });
+});
+
+var mirror; 
+// We put mirror on the global scope so it's accessible later. 
+
+draggable.on('cloned', function(clone, original, type){
+    mirror = clone;
+    console.log(type, mirror);
 })
 
+draggable.on('drop', function(el, target, source){
+    // Prevents janky scroll on touch devices 
+    $(document).off('touchstart');
 
+    var $el = $(el);
+    var cloneZone = $(mirror).offset();
+    var dropZone = $el.offset();
+    
+    console.log('clone was at', cloneZone);
+    console.log('dropzone is at', dropZone);
+
+    var offsetTop = cloneZone.top - dropZone.top;
+    var offsetLeft = cloneZone.left - dropZone.left;
+
+    console.log(offsetTop, offsetLeft);
+    
+    $el.css('transform', 'translate(' + offsetLeft + 'px, ' + offsetTop + 'px)');
+
+    setTimeout(function($el) {
+        $el.css('transition', 'all 0.5s ease-in-out');
+        $el.css('transform','translate(0px, 0px)')
+    }, 250, $el);
+
+    validateAnswer(el, target, source);
+})
+
+function isAnswerCorrect(target, source) {
+    // console.log(target,source);
+    var a = source.dataset.question;
+    var b = target.dataset.answer;
+    // console.log(a,b);
+    if (a==b) {console.log("yas"); return true} else {console.log("boo"); return false}
+}
+
+function validateAnswer(el, target, source) {
+    if ( isAnswerCorrect(target,source) ) {
+        target.classList.remove("isdraggable");
+    } else {
+        target.classList.add("incorrect");
+        source.appendChild(el);
+    }
+}
