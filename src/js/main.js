4339f32c321056aff5cd335b1e182909e97a15c0
Zygar <a@andywilkinson.co.nz>
2017-11-08 13:42:38 +1300

    swaps out Shopify's Draggable with Interact.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,1 @@
-var $ = require("jquery");
-
-$(document).ready(function(){
-    console.log("Boilerplate initialized!");
-    $("h1").on("click", function(){(alert("hi"))});
-    console.log(window.Modernizr, window.jQuery)
-})
+import interact from 'interact.js';
