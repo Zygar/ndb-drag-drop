82b039876d7d04d7bdd163b55204a079740c9bd2
Zygar <a@andywilkinson.co.nz>
2017-11-13 11:48:10 +1300

    Adds imagery for hazard matcher

--- src/js/data.js
+++ src/js/data.js
@@ -1,24 +1,24 @@
 module.exports = [
   [
-    {text: "", image: require('../img/biohazard.png')},
+    {text: "", image: require('../img/explosive.png')},
     {text: "Explosive", description: "It can suddenly explode.", image: ""}
   ],
   [
-    {text: "", image: require('../img/biohazard.png')},
+    {text: "", image: require('../img/flammable.png')},
     {text: "Flammable", description: "It can catch fire easily and it will burn quickly.", image: ""}
   ],
   [
-    {text: "", image: require('../img/biohazard.png')},
-    {text: "Oxidizing Agent", description: "A capacity to oxidise (it gives off oxygen and, in this situation, it increases the chance of fire.)", image: require('../img/biohazard.png')}
+    {text: "", image: require('../img/oxidising.png')},
+    {text: "Oxidizing Agent", description: "A capacity to oxidise (it gives off oxygen and, in this situation, it increases the chance of fire.)", image: ""}
   ],[
-    {text: "", image: require('../img/biohazard.png')},
+    {text: "", image: require('../img/corrosive.png')},
     {text: "Corrosive", description:"It will destroy or damage the surface of other things near it.", image: ""}
   ],[
-    {text: "", image: require('../img/biohazard.png')},
+    {text: "", image: require('../img/toxic.png')},
     {text: "Toxic", description:"It has the ability to harm and damage a living thing.", image: ""}
   ],
   [
-    {text: "", image: require('../img/biohazard.png')},
+    {text: "", image: require('../img/eco-waste.png')},
     {text: "Eco-toxic", description:"It is harmful to the environment, either by killing living things directly or by building up in the environment.", image: ""}
   ],
   [
