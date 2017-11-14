3c29960c07fc0a9089317e78961dedc8b9fc3c79
Zygar <a@andywilkinson.co.nz>
2017-11-14 13:45:49 +1300

    Initial commit - gets content for pharmacy medicine matcher in!

--- src/js/data.js
+++ src/js/data.js
@@ -1,28 +1,28 @@
 module.exports = [
   [
-    {text: "", image: require('../img/explosive.png')},
-    {text: "Explosive", description: "It can suddenly explode.", image: ""}
+    {text: "Panadeine tablets, 24 tablets", image: ""},
+    {text: "Secure Area Exempted Drug", description: "Pharmacist Only Medicine", image: ""}
   ],
   [
-    {text: "", image: require('../img/flammable.png')},
-    {text: "Flammable", description: "It can catch fire easily and it will burn quickly.", image: ""}
+    {text: "Amoxycillin 250mg Capsules", image:""},
+    {text: "Dispensary", description: "Prescription Medicine", image: ""}
   ],
   [
-    {text: "", image: require('../img/oxidising.png')},
-    {text: "Oxidizing Agent", description: "A capacity to oxidise (it gives off oxygen and, in this situation, it increases the chance of fire.)", image: ""}
+    {text: "Beclomethasone aqueous 50 mcg nasal spray", image: ""},
+    {text: "Retail area", description: "Pharmacy Medicine", image: ""}
   ],[
-    {text: "", image: require('../img/corrosive.png')},
-    {text: "Corrosive", description:"It will destroy or damage the surface of other things near it.", image: ""}
+    {text: "Nicotine gum", image: ""},
+    {text: "Retail area", description:"General sale medicine", image: ""}
   ],[
-    {text: "", image: require('../img/toxic.png')},
-    {text: "Toxic", description:"It has the ability to harm and damage a living thing.", image: ""}
+    {text: "Actrapid Insulin", image: ""},
+    {text: "Dispensary refrigerator", description:"", image: ""}
   ],
   [
-    {text: "", image: require('../img/eco-waste.png')},
-    {text: "Eco-toxic", description:"It is harmful to the environment, either by killing living things directly or by building up in the environment.", image: ""}
+    {text: "Hydrocortisone cream 1%, 30 g ", image: ""},
+    {text: "Retail area", description:"Pharmacist Only Medicine", image: ""}
   ],
   [
-    {text: "", image: require('../img/biohazard.png')},
-    {text: "Biohazard", description:"Can cause harm.",image: ""}
+    {text: "Methylated Spirits 100ml", image: ""},
+    {text: "Retail area", description: "Hazardous Substance – shelf 1.5m above floor",image: ""}
   ]
 ]
