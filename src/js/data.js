c7cd8fa5df674e1a4016bf59a1a6334081e972ad
Zygar <a@andywilkinson.co.nz>
2017-11-24 12:54:05 +1300

    Adds icons and visual tidiness to the medicine drag and dropper

--- src/js/data.js
+++ src/js/data.js
@@ -1,28 +1,28 @@
 module.exports = [
   [
-    {text: "Panadeine tablets, 24 tablets", image: ""},
+    {text: "Panadeine tablets, 24 tablets", image: require('../img/painkillers.png')},
     {text: "Secure Area Exempted Drug", description: "Pharmacist Only Medicine", image: ""}
   ],
   [
-    {text: "Amoxycillin 250mg Capsules", image:""},
+    {text: "Amoxycillin 250mg Capsules", image:require('../img/antibiotics.png')},
     {text: "Dispensary", description: "Prescription Medicine", image: ""}
   ],
   [
-    {text: "Beclomethasone aqueous 50 mcg nasal spray", image: ""},
+    {text: "Beclomethasone aqueous 50 mcg nasal spray", image: require('../img/nasal-spray.png')},
     {text: "Retail area", description: "Pharmacy Medicine", image: ""}
   ],[
-    {text: "Nicotine gum", image: ""},
+    {text: "Nicotine gum", image: require('../img/nicotine-gum.png')},
     {text: "Retail area", description:"General sale medicine", image: ""}
   ],[
-    {text: "Actrapid Insulin", image: ""},
+    {text: "Actrapid Insulin", image: require('../img/insulin.png')},
     {text: "Dispensary refrigerator", description:"", image: ""}
   ],
   [
-    {text: "Hydrocortisone cream 1%, 30 g ", image: ""},
+    {text: "Hydrocortisone cream 1%, 30 g ", image: require('../img/steroids.png')},
     {text: "Retail area", description:"Pharmacist Only Medicine", image: ""}
   ],
   [
-    {text: "Methylated Spirits 100ml", image: ""},
+    {text: "Methylated Spirits 100ml", image: require('../img/meths.png')},
     {text: "Retail area", description: "Hazardous Substance – shelf 1.5m above floor",image: ""}
   ]
 ]
